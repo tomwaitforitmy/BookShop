ee0d54e5e13a088265b835a66470bac1d63d58e2
Thomas Kilgus <tomwaitforitmy@gmail.com>
2021-06-03 20:53:35 +0200

    Install thunk

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import { fetchFonts } from "./common_functions/fetchFonts";
 import AppLoading from "expo-app-loading";
 import ShopNavigator from "./navigation/ShopNavigator";
 import { enableScreens } from "react-native-screens";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import productsReducer from "./store/reducers/producsReducer";
 import cartReducer from "./store/reducers/cartReducer";
 import ordersReducer from "./store/reducers/ordersReducer";
+import ReduxThunk from "redux-thunk";
 
 enableScreens();
 
@@ -17,7 +18,7 @@ const rootReducer = combineReducers({
   orders: ordersReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default function App() {
   const [fontLoaded, setFontLoaded] = useState(false);
